df6dca21f33093d1cd0c2da2b98180b7acd16ba6
MEGUEDMINI Mohamed <m.bbird21@proton.me>
2025-05-25 11:03:17 +0200

    feat WIP: add footer

---
+++ src/sections/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+	return (
+		<div className="flex flex-col gap-10 w-full py-20 bg-black text-white">
+			 <div className="w-200 bg-neutral-500 h-0.5 mx-auto"></div>
+			 <div className="flex flex-col w-auto ml-10  gap-y-8 bg-pink-900">
+				<div className=" text-2xl">
+					bbird.dev
+				</div>
+					© 2025 Mohamed Meguedmini
+			 </div>
+			<div className="w-200 bg-neutral-500 h-0.5 mx-auto"></div>
+
+		</div>
+	)
+}
+
+export default Footer;
